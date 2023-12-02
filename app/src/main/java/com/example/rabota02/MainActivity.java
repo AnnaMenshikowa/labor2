1919efcfee13b70ed00b00c9fa7607f2f40250e1
AnnaMenshikowa <130694230+AnnaMenshikowa@users.noreply.github.com>
2023-12-03 00:37:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rabota02/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.rabota02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.rabota02.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void start2Activity(View v) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+    public void start3Activity(View v) {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+    public void start4Activity(View v) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+    public void onClick(View v){
+        this.finishAffinity();
+    }
+
+}
